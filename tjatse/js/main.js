d079aaa3f20fad22f99cd120368587c89023be20
lh <skylu@bjczxd.com>
2016-12-29 15:01:19 +0800

    weChat

--- tjatse/js/main.js
+++ tjatse/js/main.js
@@ -34,17 +34,16 @@ Pace.once('hide',function(){
     /**
      * 元素加载完毕后执行
      */
-    clearInterval(loadInterval);
-    loadInterval = null;
+    //clearInterval(loadInterval);
+    //loadInterval = null;
     initPage();
-    alert('加载完毕');
+    //alert('加载完毕');
 });
 
-window.onload = function(){
-    Http = new Http(host);
-    pageLoad();
-    getSingPackage();
-}
+//window.onload = function(){
+//    Http = new Http(host);
+//    pageLoad();
+//}
 
 /**
  * 页面加载
@@ -70,101 +69,4 @@ function initPage(){
             rq = rt.rq;
         }
     }});
-}
-
-
-
-
-/**
- * 微信分享
- */
-function getSingPackage(){
-    var p ='url='+ encodeURIComponent(location.href);
-    Http.ajaxRequest({uri:'wx/signature?'+p,success:function(rt){
-        if (rt.success) {
-            var data = rt.data;
-            $("input[name=appId]").val(data.appId);
-            $('input[name=timestamp]').val(data.timestamp);
-            $('input[name=nonceStr]').val(data.nonceStr);
-            $('input[name=signature]').val(data.signature);
-
-            wxConfig();
-        }
-    }});
-}
-
-
-function wxConfig(){
-
-    wx.config({
-        debug: false,
-        appId: $("input[name=appId]").val(),
-        timestamp:$('input[name=timestamp]').val(),
-        nonceStr: $('input[name=nonceStr]').val(),
-        signature: $('input[name=signature]').val(),
-        jsApiList: [
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'onMenuShareQQ',
-            'onMenuShareWeibo'
-            // 所有要调用的 API 都要加到这个列表中
-        ]
-    });
-    wx.ready(function () {
-        // 在这里调用 API
-        wx.onMenuShareTimeline({
-            title:title, // 分享标题
-            link: link, // 分享链接
-            desc: desc, // 分享描述
-            imgUrl:imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
-
-        wx.onMenuShareAppMessage({
-            title: title, // 分享标题
-            desc: desc, // 分享描述
-            link: link, // 分享链接
-            imgUrl: imgUrl, // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
-
-        wx.onMenuShareQQ({
-            title: title, // 分享标题
-            desc: desc, // 分享描述
-            link: link, // 分享链接
-            imgUrl: imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
-
-        wx.onMenuShareWeibo({
-            title: title, // 分享标题
-            desc: desc, // 分享描述
-            link: link, // 分享链接
-            imgUrl: imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
-
-    });
 }
