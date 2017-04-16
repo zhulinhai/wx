79bd8faf1eb16409f117a077a5dac4320a03784d
zhulinhai <zhulh128@163.com>
2017-04-16 16:14:00 +0800

    infiniti项目

--- infiniti/js/data.js
+++ infiniti/js/data.js
@@ -2,10 +2,10 @@
  * Created by zhulinhai on 17/4/16.
  */
 var carsPlan = [
-    {band: 'QX30', plan: '首付低至4.99万，轻松日供20元，指定车型尊享1年0利率，更享全额购置税补税。'},
-    {band: 'QX50', plan: '日供26元，首付低至6.99万，全系尊享2年0利率，更享上海车展最高万元置换特权礼遇。'},
-    {band: 'Q50L', plan: '日供28元，首付低至5.59万，全系尊享2年0利率，更享上海车展最高万元置换特权礼遇。'},
-    {band: 'Q70L', plan: '专属0利率方案，更享上海车展全系最高万元置换特权礼遇。'},
-    {band: 'ESQ', plan: '日供18元，首付低至3.79万，更享全额购置税补贴。'},
-    {band: 'QX80', plan: '日供100元及5年超长期贷款。'}
+    {band: 'QX30', normal: 'qx30.png', hover: 'qx30-w.png', title: 'qx30-b.png', banner:'qx30.jpg', images: ['qx30-0.jpg','qx30-1.jpg','qx30-2.jpg','qx30-3.jpg','qx30-4.jpg'], plan: '首付低至4.99万，轻松日供20元，指定车型尊享1年0利率，更享全额购置税补税。'},
+    {band: 'QX50', normal: 'qx50.png', hover: 'qx50-w.png', title: 'qx50-b.png', banner:'qx50.jpg', images: ['qx50-0.jpg','qx50-1.jpg'], plan: '日供26元，首付低至6.99万，全系尊享2年0利率，更享上海车展最高万元置换特权礼遇。'},
+    {band: 'Q50L', normal: 'q50L.png', hover: 'q50L.png', title: 'q50L-b.png', banner:'q50l.jpg', images: ['q50l-0.jpg','q50l-1.jpg','q50l-2.jpg','q50l-3.jpg'], plan: '日供28元，首付低至5.59万，全系尊享2年0利率，更享上海车展最高万元置换特权礼遇。'},
+    {band: 'Q70L', normal: 'q70L.png', hover: 'q70L-w.png', title: 'q70L-b.png', banner:'q70l.jpg', images: ['q70l.jpg'], plan: '专属0利率方案，更享上海车展全系最高万元置换特权礼遇。'},
+    {band: 'ESQ', normal: 'ESQ.png', hover: 'ESQ-w.png', title: 'ESQ-b.png', banner:'esq.jpg', images: ['esq.jpg'], plan: '日供18元，首付低至3.79万，更享全额购置税补贴。'},
+    {band: 'QX80', normal: 'qx80.png', hover: 'qx80-w.png', title: 'qx80-b.png', banner:'qx80.jpg', images: ['qx80.jpg'], plan: '日供100元及5年超长期贷款。'}
 ];
