2506f6433f53020630e3eaeffa29ab01d1a50a24
lh <skylu@bjczxd.com>
2017-04-17 15:33:22 +0800

    mondeo

--- mondeo/js/main.js
+++ mondeo/js/main.js
@@ -2,5 +2,29 @@
  * Created by luhao on 2017/4/17.
  */
 (function(){
-    alert(111);
+    var host = 'http://api.bjczxda.com/api/';
+    var http = new Http(host);
+    var flag = 'MONDEO_20170501';
+
+    $('#submitComment').on('click',function(e){
+       var params = $('form:first').serialize();
+        http.ajaxRequest({
+           type:'GET',
+           uri:'h5/storeComment?' + params + '&flag='+flag,
+           success:function(json){
+               alert(json.data);
+           }
+       });
+    });
+
+    $('#submitApply').on('click',function(e){
+        var params = $('form:last').serialize();
+        http.ajaxRequest({
+            type:'GET',
+            uri:'h5/storeProposer?' + params + '&flag='+flag,
+            success:function(json){
+                alert(json.data);
+            }
+        });
+    });
 })($)
