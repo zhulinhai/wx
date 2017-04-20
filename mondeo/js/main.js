f8d0f023df201f965ff31d8b793eb38ad685ed7b
lh <skylu@bjczxd.com>
2017-04-20 17:25:01 +0800

    mondeo

--- mondeo/js/main.js
+++ mondeo/js/main.js
@@ -11,11 +11,29 @@
         tricyclic_turns:0, //三环圈数
         tetracyclic_turns:0, //四环圈数
         pentacyclic_turns:0, //五环圈数
-        used_gasoline:0, //已使用油量
-        pastTime:0, // 已挑战时间
+        used_gasoline:0, // 三环已使用油量
+        used_gasonline_tera:0, // 四环已使用油量
+        used_gasonline_penta:0, // 武汉已使用油量
+        used_time_tri:0, // 已挑战时间
         support:0, //支持挑战成功人数
-        nonsupport:0 //不支持人数
+        nonsupport:0, //不支持人数
+        active_state:0 // 活动状态 0：活动前期 1：活动中期（直播） 2：活动后期（直播结束）
     };
+    /**
+     * videojs 对象
+     * @type {null}
+     */
+    var player = null;
+
+    /**
+     * websocket server address
+     * @type {string}
+     */
+    var websocket_url =  'ws://node.bjczxda.com';
+    var socket = null;
+
+
+
     /**
      * 忽略部分ajax请求
      * @type {{ajax: {ignoreURLs: *[]}}}
@@ -33,6 +51,28 @@
     Pace.once('hide', function(e){
 
         bindEvent();
+
+        player = videojs('my-player',{
+            controls: true,
+            autoplay: false,
+            loop:true,
+            preload: 'auto'
+        });
+
+        player.src('http://vedio.yunmfang.com/K6015-480p-16-9.mp4');
+
+        // connect websocket server
+        socket = io.connect(websocket_url);
+
+        //listen the channel broadcast
+        socket.on('live_channel_'+flag , function(data){
+            console.log(data);
+            $('#code').html(data.props);
+        });
+
+        //player.on('ended',function(){
+        //    alert('play ended!');
+        //});
     });
 
     function bindEvent(){
