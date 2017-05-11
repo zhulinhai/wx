474793622629aabf9c2a05c52580aba430bc9e66
zhulinhai <zhulh128@163.com>
2017-05-11 19:39:22 +0800

    比亚迪宋元2017

--- bydsy2017/js/gamePlayer.js
+++ bydsy2017/js/gamePlayer.js
@@ -3,28 +3,23 @@
  */
 const imgWidth = 1280, imgHeight = 1055;
 const videoUrl = 'http://vedio.yunmfang.com/bydVideo2017-480p.mp4';
-let gamePlayer = {
+const postUrl = 'src/placeHolder.png';
+const $videoBox = $('.video-js-box');
+var gamePlayer = {
     imgWidth: 1280,
     imgHeight: 1055,
     bgScale: 1,
     myScroll: null,
-    keyList: [1, 3, 5],
+    keyList: [0, 1, 2],
     getKeyList: [],
     boxPoints: [
-        {'index': 0,'el': '.box1','x': 210, 'y': 312, 'isOpened': false},
-        {'index': 1,'el': '.box2','x': 210, 'y': 640, 'isOpened': false},
-        {'index': 2,'el': '.box3','x': 450, 'y': 492, 'isOpened': false},
-        {'index': 3,'el': '.box4','x': 780, 'y': 550, 'isOpened': false},
-        {'index': 4,'el': '.box5','x': 1000, 'y': 142, 'isOpened': false},
-        {'index': 5,'el': '.box6','x': 1080, 'y': 420, 'isOpened': false},
-        {'index': 6,'el': '.box7','x': 1124, 'y': 934, 'isOpened': false}
+        {'index': 0,'el': '.box1','x': 195, 'y': 295, 'isOpened': false},
+        {'index': 1,'el': '.box2','x': 765, 'y': 535, 'isOpened': false},
+        {'index': 2,'el': '.box3','x': 1070, 'y': 410, 'isOpened': false}
     ],
     init: function () {
         this.bgScale = $(window).height() /imgHeight;
-        this.fixPageSize();
-        this.initScroll();
-        this.initKeys();
-        this.initBoxes();
+        this.bindClicks();
     },
     initScroll: function () {
         this.myScroll = new IScroll('#wrapper', {
@@ -35,44 +30,84 @@ let gamePlayer = {
             bounce: false
         });
     },
+    bindClicks: function () {
+        var that = this;
+        /*点击开启盖世宝藏*/
+        $('#btnStartGame').click(function () {
+            $('.section-1').hide();
+            $('.section-2').show();
+            that.fixPageSize();
+            that.initScroll();
+            that.initBoxes();
+        });
+        /*点击活动规则按钮*/
+        $('#btnActRule').click(function () {
+            $('#actRuleDialog').show();
+        });
+        /*关闭留资抽奖对话框*/
+        $('#closeInfoDialog').click(function () {
+            $('#infoDialog').hide();
+        });
+        /*关闭视频弹出框*/
+        $('#closeVideoDialog').click(function () {
+            $('#videoPlayer').get(0).pause();
+            $('#videoDialog').hide();
+        });
+        /*关闭车型亮点弹出框*/
+        $('#closeCarInfoDialog').click(function () {
+            $('#carInfoDialog').hide();
+        });
+        /*关闭活动规则页面*/
+        $('#closeActRuleDialog').click(function () {
+            $('#actRuleDialog').hide();
+        });
+        /*关闭结果提示框*/
+        $('#closeTipResultDialog').click(function () {
+            $('#tipResultDialog').hide();
+        });
+        $('#closeUserInfoDialog').click(function () {
+            $('#userInfoDialog').hide();
+        });
+
+        $('#btnSubmit').click(submitInfo);
+    },
     fixPageSize: function () {
+        /*播放器相关操作*/
+        $('#videoPlayer').width($videoBox.width()).height($videoBox.height()).attr({'src': videoUrl,'poster': postUrl});
         $('#game-box').width(this.bgScale * imgWidth);
     },
-    initKeys: function () {
-        let list = [];
-        let sum = 0;
-        /*产生三个随机数*/
-        for (let i = 0; i< 3; i ++) {
-            let randomNum = Math.floor(Math.random() * this.boxPoints.length);
-            if ($.inArray(randomNum, list) < 0) {
-                list.push(randomNum);
-                sum += randomNum;
-            }
-        }
-        /*如果产生相同随机数，则采用默认宝箱位置*/
-        /*排除首屏三个宝箱全部开启情况*/
-        if (list.length >= 3 && sum > 3) {
-            this.keyList = list;
-        }
-        console.log(this.keyList);
-    },
     initBoxes: function () {
+        var that = this;
         this.boxPoints.forEach(function (e) {
             $(e.el).css({
                 'top': e.y * gamePlayer.bgScale ,
                 'left': e.x * gamePlayer.bgScale
             }).click(function () {
-                console.log('index:' + e.index + ' keyList:' + gamePlayer.keyList);
-                if ($.inArray(e.index, gamePlayer.keyList) >= 0) {
-                    gamePlayer.boxPoints[e.index].getKey = true;
+                if (e.index == 0) {
+                    $('#videoDialog').show();
+                } else if (e.index == 1) {
+                    $('#carInfoDialog').show();
+                } else if (e.index == 2) {
+                    $('#actRuleDialog').show();
                 }
-                gamePlayer.boxPoints[e.index].isOpened = true;
-                console.log(gamePlayer.boxPoints);
+
+                if (!that.boxPoints[e.index].isOpened ) {
+                    that.boxPoints[e.index].isOpened = true;
+                    /*todo 展示宝箱打开动画 */
+                    that.getKeyList.push(e.index);
+                    that.updateKeyList();
+                }
+                console.log(that.boxPoints);
             });
 
         });
     },
-    showVideoDialog: function() {
+    updateKeyList: function () {
+        var content = '';
+        for (var i =0;i< this.keyList.length; i ++) {
+            content += '<li><img src="' + (i < this.getKeyList.length?'src/2-key-hover.png': 'src/2-key.png') + '"/></li>';
+        }
+        $('#keyList').html(content);
 
     }
 };
