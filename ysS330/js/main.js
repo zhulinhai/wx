1439dcf8c1b26f1d5e96dd4dcc9e2b4d40172f1d
zhulinhai <zhulh128@163.com>
2017-08-29 10:35:27 +0800

    驭胜S330

--- ysS330/js/main.js
+++ ysS330/js/main.js
@@ -2,44 +2,15 @@
  * Created by zhulinhai on 17/8/28.
  */
 
-requirejs.config({
-    baseUrl: 'js/lib',
-    paths: {
-        app: '../app'
-    },
-    shim: {
-        backbone: {
-            deps: ['pace','jquery-1.12.1.min', 'underscore'],
-            exports: 'Backbone'
-        },
-        underscore: {
-            exports: '_'
-        }
-    }
-});
 
-define(['pace'], function () {
-    paceOptions = {
+define(['pace'], function(pace){
+    pace.start({
         elements: true,
+        ajax: false,
         restartOnRequestAfter: false
-    };
-
-    Pace.once('start',function(){
-        let curProgress = 0, myInterval = -1;
-        myInterval = setInterval(function(){
-            let progress = parseInt(document.querySelectorAll('.pace-progress')[0].getAttribute("data-progress"));
-            if (progress > curProgress) {
-                document.getElementById('loading-percent').innerHTML = progress + '%';
-                curProgress = progress;
-            }
-        },100);
     });
 
-    Pace.once('hide', function () {
-        clearInterval(myInterval);
-        myInterval = -1;
-
+    pace.stop(function () {
         console.log('hide');
     });
 });
-
