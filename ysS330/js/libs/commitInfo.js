3c5156179bba52d273622310a142142a08fc57f1
zhulinhai <zhulh128@163.com>
2017-08-30 16:23:15 +0800

    驭胜S330-留资和视频测试

--- ysS330/js/libs/commitInfo.js
+++ ysS330/js/libs/commitInfo.js
@@ -3,59 +3,98 @@
  */
 
 require.config({
-
-    // RequireJS 通过一个相对的路径 baseUrl来加载所有代码。baseUrl通常被设置成data-main属性指定脚本的同级目录。
-    baseUrl: "./",
-
-    // 第三方脚本模块的别名,jquery比libs/jquery-1.11.1.min.js简洁明了；
+    baseUrl: "./js/libs",
     paths: {
-        "jquery": "./jquery-1.12.1.min.js"
+        "jquery": "./jquery-1.12.1.min",
+        "helper": './helper'
     }
-
 });
 
-
-require(['helper','jquery'], function (helper, $) {
+define(['helper', 'jquery'], function (helper, $) {
     const host = 'http://api.bjczxda.com/api';
-    let name, mobile, province,city, dealer;
-    name='祝先生';//trim($("input[name='realname']").val());
-    mobile='18519002237';//trim($("input[name='mobile']").val());
-    province ='湖北省';//trim($("#province").find("option:selected").text());
-    city ='武汉市';//trim($("#city").find("option:selected").text());
-    dealer='大啊啊啊';//trim($("#dealer").find("option:selected").text());
-    if(helper.isNullOrEmpty(name)) {
-        alert("请填写姓名!");
-        return 0;
-    }else if(!helper.checkIsMobile(mobile)){
-        alert("请输入正确的手机号!");
-        return 0;
-    }else if(province.indexOf("请选择")>=0){
-        alert("请选择省份");
-        return 0;
-    }else if(city.indexOf("请选择")>=0){
-        alert("请选择城市");
-        return 0;
-    }else if(dealer.indexOf("请选择")>=0){
-        alert("请选择经销商");
-        return 0;
-    }
-    let flag = 'yss330';//trim($("input[name='flag']").val());
-    let url = host + '/ysS330/luckyDraw?flag=' + flag +'&name='+ name +'&mobile=' + mobile + '&province='+ province + '&city=' + city + '&dealer=' + dealer;
+    let commit = {};
+    let flag, name, mobile, province, city, dealer;
+    commit.bindInfo = function () {
+        $.get("./js/libs/data.json",function(data,status){
+            if (status == 'success') {
+                const $province = $('#province'),
+                    $city = $('#city'),
+                    $dealer = $('#dealer');
+                let provinces = data.province,
+                    cities = data.city,
+                    dealers = data.dealer;
 
-    $.ajax({
-        type: "get",
-        url: url,
-        dataType: "jsonp",
-        success: function(data){
-            var response = eval('(data)');
-            if (response.success) {
+                $province.change(function(){
+                    $city.empty().html('<option>请选择</option>');
+                    $dealer.empty().html('<option>请选择</option>');
 
-            } else {
-                alert(response.message);
+                    if($province.val() != '请选择'){
+                        for(let j = 0; j < data.city.length; j++)
+                            if(cities[j].p == $('#province').val())
+                                $city.append('<option>'+cities[j].c+'</option>');
+                    }
+                });
+
+                $city.change(function(){
+                    $dealer.empty().html('<option>请选择</option>');
+                    if($city.val() != '请选择'){
+                        for(let j = 0; j < dealers.length; j++)
+                            if(dealers[j].d == $city.val())
+                                $dealer.append('<option>'+dealers[j].c +'</option>');
+                    }
+                });
+
+                for(let i = 0; i < provinces.length; i ++) $province.append('<option>'+provinces[i].p+'</option>');
             }
-        },
-        error: function(data){
-            alert("加载超时,请检查网络连接");
+        });
+        $('#btnSubmit').click(this.submitInfo);
+
+    };
+
+    commit.submitInfo = function () {
+        flag = helper.trim($("input[name='flag']").val());
+        name = helper.trim($("input[name='realname']").val());
+        mobile = helper.trim($("input[name='mobile']").val());
+        province = helper.trim($("#province").find("option:selected").text());
+        city = helper.trim($("#city").find("option:selected").text());
+        dealer = helper.trim($("#dealer").find("option:selected").text());
+
+        if (helper.isNullOrEmpty(name)) {
+            alert("请填写姓名!");
+            return 0;
+        } else if (!helper.checkIsMobile(mobile)) {
+            alert("请输入正确的手机号!");
+            return 0;
+        } else if (province.indexOf("请选择") >= 0) {
+            alert("请选择省份");
+            return 0;
+        } else if (city.indexOf("请选择") >= 0) {
+            alert("请选择城市");
+            return 0;
+        } else if (dealer.indexOf("请选择") >= 0) {
+            alert("请选择经销商");
+            return 0;
         }
-    });
+
+        let url = host + '/ysS330/luckyDraw?flag=' + flag + '&name=' + name + '&mobile=' + mobile + '&province=' + province + '&city=' + city + '&dealer=' + dealer;
+        $.ajax({
+            type: "post",
+            url: url,
+            dataType: "json",
+            success: function (data) {
+                let response = eval('(data)');
+                console.log(response);
+                if (response.success) {
+
+                } else {
+                    alert(response.message);
+                }
+            },
+            error: function (data) {
+                alert("加载超时,请检查网络连接");
+            }
+        });
+    };
+
+    return commit;
 });
--- ysS330/js/main.js
+++ ysS330/js/main.js
@@ -2,56 +2,16 @@
  * Created by zhulinhai on 17/8/28.
  */
 require.config({
-    // RequireJS 通过一个相对的路径 baseUrl来加载所有代码。baseUrl通常被设置成data-main属性指定脚本的同级目录。
     baseUrl: "./js",
-    // 第三方脚本模块的别名,jquery比libs/jquery-1.11.1.min.js简洁明了；
     paths: {
         "jquery": "./libs/jquery-1.12.1.min",
-        "helper": './libs/helper'
+        'commit': './libs/commitInfo'
     }
 });
 
-require(['helper','jquery'], function (helper, $) {
-    let name, mobile, province,city, dealer;
-    name='祝先生';//trim($("input[name='realname']").val());
-    mobile='18519002237';//trim($("input[name='mobile']").val());
-    province ='湖北省';//trim($("#province").find("option:selected").text());
-    city ='武汉市';//trim($("#city").find("option:selected").text());
-    dealer='大啊啊啊';//trim($("#dealer").find("option:selected").text());
-    if(helper.isNullOrEmpty(name)) {
-        alert("请填写姓名!");
-        return 0;
-    }else if(!helper.checkIsMobile(mobile)){
-        alert("请输入正确的手机号!");
-        return 0;
-    }else if(province.indexOf("请选择")>=0){
-        alert("请选择省份");
-        return 0;
-    }else if(city.indexOf("请选择")>=0){
-        alert("请选择城市");
-        return 0;
-    }else if(dealer.indexOf("请选择")>=0){
-        alert("请选择经销商");
-        return 0;
-    }
+require(['commit','jquery'], function (commit, $) {
+    <!-- 预约时间绑定信息 -->
+    commit.bindInfo();
 
-    let flag = 'yss330';//trim($("input[name='flag']").val());
-    const host = 'http://api.bjczxda.com/api';
-    let url = host + '/ysS330/luckyDraw?flag=' + flag +'&name='+ name +'&mobile=' + mobile + '&province='+ province + '&city=' + city + '&dealer=' + dealer;
-    $.ajax({
-        type: "post",
-        url: url,
-        dataType: "json",
-        success: function(data){
-            let response = eval('(data)');
-            if (response.success) {
 
-            } else {
-                alert(response.message);
-            }
-        },
-        error: function(data){
-            alert("加载超时,请检查网络连接");
-        }
-    });
 });
