1b6516b82d0786d4f37db1c484da8d7a6193cce9
lh <skylu@bjczxd.com>
2017-08-31 17:53:41 +0800

    'lfx2'

---
+++ lfx2/js/isAutoPlay.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-08-31 17:09:46
+ * @version $Id$
+ */
+
+       function c() {
+			var t = navigator.userAgent,
+				i = !! t.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+			return i
+		}
+		function l() {
+			var t = navigator.userAgent;
+			return "MicroMessenger" == t.match(/MicroMessenger/i)
+		}
+		function f() {
+			var t = navigator.userAgent;
+			return "QQ/" == t.match(/QQ\//i)
+		}
+		function p() {
+			var t = window.navigator.userAgent.toLowerCase(),
+				i = "";
+			if (t.indexOf("iphone") >= 0) return i = t.indexOf("os 10") >= 0 ? "ios10s" : t.indexOf("os 9") >= 0 ? "ios9s" : t.indexOf("os 8") >= 0 ? "ios8" : "iosOther"
+		}
+		function v(t) {
+			l() && c() ? "object" == typeof WeixinJSBridge ? WeixinJSBridge.invoke("WeixinJSBridgeReady", {}, function(i) {
+				t()
+			}) : document.addEventListener("WeixinJSBridgeReady", function(i) {
+				t()
+			}, !1) : t()
+		}
+		
+		function isVideoCanAutoPlay() {
+			var t = !1;
+			return c() && (l() || f()) && (t = "ios8" != p() || "iosOther" != p()), t
+		}
