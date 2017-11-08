be63b2f08c3b81fa5f65640c66daa60c308dbb2f
skylu <skylu@bjczxd.com>
2017-11-08 18:24:40 +0800

    'karry'

--- karry20171102/js/main.js
+++ karry20171102/js/main.js
@@ -14,19 +14,23 @@ var canvas, stage;
 var provinces,cities,dealers;
 var host = 'http://api.bjczxda.com/api/';
 var validate = new InputUtil();
+var INITWIDTH = 1338;
+var INITHEIGHT = 622;
 
 var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
 
 function init() {
-	canvas = document.getElementById("canvas");
-	anim_container = document.getElementById("animation_container");
-	dom_overlay_container = document.getElementById("dom_overlay_container");
-	images = images||{};
-	ss = ss||{};
-	var loader = new createjs.LoadQueue(false);
-	loader.addEventListener("fileload", handleFileLoad);
-	loader.addEventListener("complete", handleComplete);
-	loader.loadManifest(lib.properties.manifest);
+	 var w =  document.documentElement.clientWidth;
+	 var h = document.documentElement.clientHeight;
+	// load();
+	// canvas = document.getElementById("canvas");
+	// images = images||{};
+	// ss = ss||{};
+	// var loader = new createjs.LoadQueue(false);
+	// loader.addEventListener("fileload", handleFileLoad);
+	// loader.addEventListener("complete", handleComplete);
+	// loader.loadManifest(lib.properties.manifest);
+	alert(devicePixelRatio + ' ' + document.documentElement.style.fontSize + ' w:' + w + ' h:' + h + ' pw: ' + $('.pop').width() + ' ph:'+ $('.pop').height());
 }
 
 function handleFileLoad(evt) {	
@@ -45,6 +49,7 @@ function handleComplete(evt) {
 	stage.addChild(exportRoot);	
 	
 	//Registers the "tick" event listener.
+
 	fnStartAnimation = function() {
 		createjs.Ticker.setFPS(lib.properties.fps);
 		createjs.Ticker.addEventListener("tick", stage);
@@ -53,34 +58,47 @@ function handleComplete(evt) {
 	function makeResponsive(isResp, respDim, isScale, scaleType) {		
 		var lastW, lastH, lastS=1;		
 		window.addEventListener('resize', resizeCanvas);		
-		resizeCanvas();		
-		function resizeCanvas() {			
-			var w = lib.properties.width, h = lib.properties.height;			
-			var iw = window.innerWidth, ih=window.innerHeight;			
-			var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
-			if(isResp) {                
-				if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
-					sRatio = lastS;                
-				}				
-				else if(!isScale) {					
-					if(iw<w || ih<h)						
-						sRatio = Math.min(xRatio, yRatio);				
-				}				
-				else if(scaleType==1) {					
-					sRatio = Math.min(xRatio, yRatio);				
-				}				
-				else if(scaleType==2) {					
-					sRatio = Math.max(xRatio, yRatio);				
-				}			
-			}			
-			canvas.width = w*pRatio*sRatio;			
-			canvas.height = h*pRatio*sRatio;
-			canvas.style.width = dom_overlay_container.style.width = anim_container.style.width =  w*sRatio+'px';				
-			canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h*sRatio+'px';
-			// stage.scaleX = pRatio*sRatio;			
-			// stage.scaleY = pRatio*sRatio;			
-			lastW = iw; lastH = ih; lastS = sRatio;		
+		resizeCanvas();
+		function resizeCanvas(){
+		    stageWidth =  document.documentElement.clientWidth;
+		    stageHeight = document.documentElement.clientHeight;
+		    stageScale = stageWidth/(INITWIDTH/2);
+		    canvas.style.width = INITWIDTH/2*stageScale + 'px';
+		    canvas.style.height = INITHEIGHT / 2 * stageScale + 'px';
+		    canvas.style.left = 0 + 'px';
+		    canvas.style.top =0+ 'px';
 		}
+		// resizeCanvas();
+		//宽度自适应	
+		// function resizeCanvas() {			
+		// 	var w = lib.properties.width, h = lib.properties.height;			
+		// 	var iw = window.innerWidth, ih=window.innerHeight;			
+		// 	var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		// 	if(isResp) {                
+		// 		if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+		// 			sRatio = lastS;                
+		// 		}				
+		// 		else if(!isScale) {					
+		// 			if(iw<w || ih<h)						
+		// 				sRatio = Math.min(xRatio, yRatio);				
+		// 		}				
+		// 		else if(scaleType==1) {					
+		// 			sRatio = Math.min(xRatio, yRatio);				
+		// 		}				
+		// 		else if(scaleType==2) {					
+		// 			sRatio = Math.max(xRatio, yRatio);				
+		// 		}			
+		// 	}			
+		// 	canvas.width = w*pRatio*sRatio;			
+		// 	canvas.height = h*pRatio*sRatio;
+		// 	canvas.style.width =  w*sRatio+'px';				
+		// 	canvas.style.height = h*sRatio+'px';
+		// 	// canvas.style.width = dom_overlay_container.style.width = anim_container.style.width =  w*sRatio+'px';				
+		// 	// canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h*sRatio+'px';
+		// 	stage.scaleX = pRatio*sRatio;			
+		// 	stage.scaleY = pRatio*sRatio;			
+		// 	lastW = iw; lastH = ih; lastS = sRatio;		
+		// }
 	}
 	makeResponsive(false,'both',false,1);	
 	fnStartAnimation();
@@ -148,11 +166,11 @@ function load(){
 
 function resize(){
     //调整留资框
-	var scale = document.body.clientWidth / 375;
-	var popH = $('.pop').height();
-	var popT = parseInt($('.pop').css('margin-top'));
-	$('.pop').css('transform','scale(' + scale + ','+ scale + ')');
-	$('.pop').css('margin-top', (popT + (popH * (scale - 1))/2 ) +'px');
+	// var scale = document.body.clientWidth / 375;
+	// var popH = $('.pop').height();
+	// var popT = parseInt($('.pop').css('margin-top'));
+	// $('.pop').css('transform','scale(' + scale + ','+ scale + ')');
+	// $('.pop').css('margin-top', (popT + (popH * (scale - 1))/2 ) +'px');
 	// alert('width: ' + document.body.clientWidth + ' height: ' + document.body.clientHeight);
 }
 
