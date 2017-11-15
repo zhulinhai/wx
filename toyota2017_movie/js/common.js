d2bc7d26e1e78400390d543d91e93b805abc8015
zhulinhai <zhulh128@163.com>
2017-11-15 15:52:41 +0800

    丰田金融

--- toyota2017_movie/js/common.js
+++ toyota2017_movie/js/common.js
@@ -1,6 +1,59 @@
 /*
 * 工具函数
 * */
+function getPixelRatio(context) {
+    var backingStore = context.backingStorePixelRatio ||
+        context.webkitBackingStorePixelRatio ||
+        context.mozBackingStorePixelRatio ||
+        context.msBackingStorePixelRatio ||
+        context.oBackingStorePixelRatio ||
+        context.backingStorePixelRatio || 1;
+    return (window.devicePixelRatio || 1) / backingStore;
+}
+
+function getYear(dateStr) {
+
+    return '';
+}
+
+function getMonth(dateStr) {
+
+    return '';
+}
+
+function getTrueLength(str){//获取字符串的真实长度（字节长度）
+    var len = str.length, truelen = 0;
+    for(var x = 0; x < len; x++){
+        if(str.charCodeAt(x) > 128){
+            truelen += 2;
+        }else{
+            truelen += 1;
+        }
+    }
+    return truelen;
+}
+function cutString(str, leng){//按字节长度截取字符串，返回substr截取位置
+    var len = str.length, tlen = len, nlen = 0;
+    for(var x = 0; x < len; x++){
+        if(str.charCodeAt(x) > 128){
+            if(nlen + 2 < leng){
+                nlen += 2;
+            }else{
+                tlen = x;
+                break;
+            }
+        }else{
+            if(nlen + 1 < leng){
+                nlen += 1;
+            }else{
+                tlen = x;
+                break;
+            }
+        }
+    }
+    return tlen;
+}
+
 function getCookie(c_name)
 {
     if (document.cookie.length>0)
